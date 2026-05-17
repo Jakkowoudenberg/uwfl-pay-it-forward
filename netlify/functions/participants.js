01a8d8044f25abe2c70908ab9e4addd724a8b63c
Jakkowoudenberg <stopdoingshityoudontlike@gmail.com>
2026-05-17 11:34:23 +0200

    Create participants.js

---
+++ netlify/functions/participants.js
@@ -0,0 +1,39 @@
+exports.handler = async function(event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  try {
+    const response = await fetch(
+      `${process.env.SUPABASE_URL}/rest/v1/registrations?select=name,company,country,type&order=country.asc,name.asc`,
+      {
+        headers: {
+          'apikey': process.env.SUPABASE_ANON_KEY,
+          'Authorization': `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(data)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
