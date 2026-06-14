f1af64ae3d87e6153e03a72c56f5d3b06e2162dc
Jakkowoudenberg <stopdoingshityoudontlike@gmail.com>
2026-06-14 12:51:48 +0200

    fix: show approved OR null status participants

--- netlify/functions/participants.js
+++ netlify/functions/participants.js
@@ -11,7 +11,7 @@ exports.handler = async function(event, context) {
 
   try {
     const response = await fetch(
-      `${process.env.SUPABASE_URL}/rest/v1/registrations?select=name,company,country,type,message,photo_url&status=eq.approved&order=country.asc,name.asc`,
+      `${process.env.SUPABASE_URL}/rest/v1/registrations?select=name,company,country,type,message,photo_url&or=(status.eq.approved,status.is.null)&order=country.asc,name.asc`,
       {
         headers: {
           'apikey': process.env.SUPABASE_ANON_KEY,
